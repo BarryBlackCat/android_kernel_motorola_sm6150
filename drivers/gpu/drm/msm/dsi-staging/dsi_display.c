e469b8bac1624cc09ef4a0ff7326f36e47c5e37e
Sultan Alsawaf <sultan@kerneltoast.com>
2018-11-25 18:23:20 -0800

    drm/msm/dsi-staging: Don't bail out when debugfs creation fails
    There's no reason why the absence of debugfs should cause the display
    driver to break. Fix it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -5693,11 +5693,7 @@ static int dsi_display_bind(struct device *dev,
 		goto error;
 	}
 
-	rc = dsi_display_debugfs_init(display);
-	if (rc) {
-		pr_err("[%s] debugfs init failed, rc=%d\n", display->name, rc);
-		goto error;
-	}
+	dsi_display_debugfs_init(display);
 
 	atomic_set(&display->clkrate_change_pending, 0);
 	atomic_set(&display->fod_ui, false);
